82c5da4a113c2b15484e9c30b985e329800a66af
joshuachilds <jhchilds@uvm.edu>
2019-02-14 23:40:24 -0500

    inital file upload

---
+++ nav.php
@@ -0,0 +1,41 @@
+<nav class = "navbar navbar-expand-lg navbar-dark">
+
+      <a href = "#" class = "navbar-brand ml-3 "> <span style = "color:#f3f3f3;">ADD-</span><span style = "color:#f3f3f3;">ON-</span><span style = "color:green;">HIGH</span> </a>
+
+      <button class = "navbar-toggler" type="button" data-toggle="collapse" data-target = "#navbarMenu"
+      aria-controls="navbarMenu" aria-expanded = "false" aria-label="Toggle Navigation">
+
+      <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- <div class="collapse navbar-collapse"></div>  THIS "CENTERS THE NAV"-->
+      <!-- Below is code for the collapsable navbarMenu, try resizing page so you you can see the "hamburger" button.-->
+      <div class="collapse navbar-collapse" id="navbarMenu">
+        <ul class="navbar-nav mr-auto" >
+          <li class="nav-item active">
+            <a href="index.php" class="nav-link">Home </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Inactive</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Inactive</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Inactive</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Inactive</a>
+          </li>
+
+        </ul>
+
+        <form action = "#" method="get" class="form-inline my-2 my-lg-0">
+          <button class="btn menu-right-btn border" type="submit">
+            PODCASTS
+          </button>
+        </form>
+
+      </div>
+
+    </nav>
---
+++ top.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+     <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+
+     <meta name="author" content="Joshua Childs">
+     <meta name="description" content="ADD on HIGH, a podcast for you." >
+     <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+     <link rel="stylesheet" href="css/custom.css" >
+     <title>ADD on HIGH</title>
+  </head>
+  <body>
