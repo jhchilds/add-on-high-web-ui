c49fe3a403392b30c6aaed962cc2e4bdd732e8d9
joshuachilds <jhchilds@uvm.edu>
2019-02-16 20:32:11 -0500

    streamlining index with better image size and php

--- index.php
+++ index.php
@@ -1,75 +1,56 @@
 <?php
-    include("top.php");
-    include("nav.php");
+include("top.php");
+include("nav.php");
+
+$directory = "/Users/joshuachilds/Desktop/undergrad_uvm/web-dev/add-on-high/images";
+
+
+
+$files = scandir($directory);
+
+$chunky_files = array_chunk($files, count($files) / 4);
 ?>
+<main class='row content'>
+
+    <?php
+    print "<section class='column index-image'>\n";
+    for ($i = 2; $i <= 8; $i++) {
+        print "<img src= 'images/" . $files[$i] . "' > \n";
+    }
+    print "</section>\n";
+
+    print "<section class='column index-image'>\n";
+    for ($i = 9; $i <= 15; $i++) {
+        print "<img src= 'images/" . $files[$i] . "' > \n";
+    }
+    print "</section>\n";
+
+    print "<section class='column index-image'>\n";
+    for ($i = 16; $i <= 22; $i++) {
+        print "<img src= 'images/" . $files[$i] . "' > \n";
+    }
+    print "</section>\n";
+
+    print "<section class='column index-image'>\n";
+    for ($i = 23; $i <= 29; $i++) {
+        print "<img src= 'images/" . $files[$i] . "' > \n";
+    }
+    print "</section>\n";
+    ?>
 
 
 
+</main>
 
-    <!-- Photo Grid -->
-    <main class="row content">
-      <section class="column index-image">
-        <img src="/images/DSC_0009.jpg" style="width:100%">
-        <img src="/images/DSC_0047.jpg" style="width:100%">
-        <img src="/images/DSC_0110.jpg" style="width:100%">
-        <img src="/images/DSC_0230.jpg" style="width:100%">
-        <img src="/images/DSC_0260.jpg" style="width:100%">
-        <img src="/images/DSC_0288.jpg" style="width:100%">
-        <img src="/images/DSC_0353.jpg" style="width:100%">
-        <img src="/images/DSC_0338.jpg" style="width:100%">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/scrollreveal"></script>
+<script src="../javascript/smooth_scrolling.js"></script>
+<script src="../javascript/jquery-3.3.1.min.js"></script>
 
-
-
-      </section>
-      <section class="column index-image">
-        <img src="/images/DSC_0344.jpg" style="width:100%">
-        <img src="/images/DSC_0255.jpg" style="width:100%">
-
-        <img src="/images/DSC_0420.jpg" style="width:100%">
-        <img src="/images/DSC_0449.jpg" style="width:100%">
-        <img src="/images/DSC_0478.jpg" style="width:100%">
-        <img src="/images/DSCN0305.JPG" style="width:100%">
-
-      </section>
-      <section class="column index-image">
-        <img src="/images/DSCN0311.JPG" style="width:100%">
-        <img src="/images/DSCN0321.JPG" style="width:100%">
-        <img src="/images/DSCN0460.JPG" style="width:100%">
-        <img src="/images/DSCN0551.JPG" style="width:100%">
-        <img src="/images/DSCN0571.JPG" style="width:100%">
-        <img src="/images/DSCN0589.JPG" style="width:100%">
-        <img src="/images/DSCN0606.JPG" style="width:100%">
-      </section>
-      <section class="column index-image">
-        <img src="/images/DSCN0620.JPG" style="width:100%">
-        <img src="/images/DSCN0630.JPG" style="width:100%">
-        <img src="/images/DSCN0717.JPG" style="width:100%">
-        <img src="/images/DSCN0718.JPG" style="width:100%">
-        <img src="/images/DSCN0799.JPG" style="width:100%">
-        <img src="/images/DSCN0829.JPG" style="width:100%">
-        <img src="/images/DSCN0850.JPG" style="width:100%">
-
-      </section>
-    </main>
-
-
-
-
-
-
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/scrollreveal"></script>
-    <script src="../javascript/smooth_scrolling.js"></script>
-    <script src="../javascript/jquery-3.3.1.min.js"></script>
-    <!-- <script src="../javascript/redirect.js"></script> -->
-
-
-
-    <script>
-    window.sr = ScrollReveal({duration : 1500});
+<script>
+    window.sr = ScrollReveal({duration: 1500});
     sr.reveal('.site-content .d-flex');
     sr.reveal('.site-content');
     sr.reveal('.navbar .navbar-brand');
@@ -82,9 +63,9 @@
     sr.reveal('.section-2c img');
     sr.reveal('.section-3 .col-md-4');
     sr.reveal('.section-4 .col-md-7, .col-md-5');
-    </script>
+</script>
 
 
-  </body>
-  </html>
+</body>
+</html>
 <!--****************************START SECTION 1******************************-->
